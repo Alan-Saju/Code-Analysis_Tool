933a16685b899a4c62026c05dbce3eace4f92eb9
Alan Saju <alansaju10x@gmail.com>
2024-12-07 19:33:13 +0530

    Used enviornment varibles to hide the API key

--- analyzer/ai_suggestions.py
+++ analyzer/ai_suggestions.py
@@ -1,8 +1,10 @@
 import google.generativeai as genai
+import os
 
 def get_optimization_suggestions(code: str) -> list:
     
-    genai.configure(api_key="AIzaSyClCyMoI5M4Q5E9fOUDOcUSdZ_IGztUfS8")
+    api_key = os.getenv("API_KEY")
+    genai.configure(api_key=api_key)
     model=genai.GenerativeModel("gemini-1.5-flash")
     suggestions =model.generate_content("identify potential performance bottlenecks and  Provides suggestions for optimization"+code)
 
